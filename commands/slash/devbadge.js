e6dc0461f3c4f1c2275b1c19ad22e5e8753bd39a
In Sovisa <insovisa@gmail.com>
2025-09-08 13:01:13 +0000

    kdit

--- commands/slash/devbadge.js
+++ commands/slash/devbadge.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { isAdmin } = require('../utils/adminCheck'); // Make sure this path is correct
+const { isAdmin } = require('../../utils/adminCheck'); // FIXED PATH: changed from ../ to ../../
 
 module.exports = {
     data: new SlashCommandBuilder()
